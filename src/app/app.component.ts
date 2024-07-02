662e90b3f6693f8212a2496c15a6b9bb9ca8d093
John Paxton <johnpaxton@gmail.com>
2024-07-02 10:19:27 -0400

    Week 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar.component';
+import { UIRouterModule } from '@uirouter/angular';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	templateUrl: './app.component.html',
+	imports: [NavbarComponent, UIRouterModule],
+	styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ui-router-demos';
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { UIRouter, UIRouterModule } from '@uirouter/angular';
+import { appRouting } from './app.routing';
+import { Visualizer } from '@uirouter/visualizer';
 
-import { routes } from './app.routes';
+function setupUIRouterConfig(router: UIRouter) {
+	// Adds the visualizer
+	router.plugin(Visualizer);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+	providers: [
+		provideHttpClient(withFetch()),
+		importProvidersFrom(
+			UIRouterModule.forRoot({ states: appRouting, config: setupUIRouterConfig })
+		),
+	],
 };
